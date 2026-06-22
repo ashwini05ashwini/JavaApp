4930fe6128bcdfe6ef8874f74c240b4e7e1b90ff
ashwini05ashwini <ashwinihalandur@gmail.com>
2026-06-22 11:12:54 +0530

    Updated maven project

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,10 +1,10 @@
 package com.example;
 
-public class App {
+public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub S
-		System.out.println("Hello, CI");
+		System.out.println("Hello World");
 
 	}
 
